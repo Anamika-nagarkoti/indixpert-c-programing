b286c4c7d79fd6e76f93cfb409534253b7ef7eb2
Anamika-nagarkoti <anamikanagarkoti@gmail.com>
2026-06-12 15:03:31 -0700

    initial setup

---
+++ sum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int number1;
+    int number2;
+    int sum;
+
+    printf("enter first number: ");
+    scanf("%d", &number1);
+
+    printf("enter second number: ");
+    scanf("%d", &number2);
+
+   sum = number1 + number2;
+
+   printf("sum is %d\n", sum);
+
+
+return 0;
+
+}
