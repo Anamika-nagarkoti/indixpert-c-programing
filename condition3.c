68f19ac5a8064b62dd8b7e056b417a4bcb6c0407
Anamika-nagarkoti <anamikanagarkoti@gmail.com>
2026-06-17 10:16:58 -0700

    array task

---
+++ condition3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int array1[5] = {1, 3, 4, 5, 3};
+    int array2[3] = {4, 3, 2};
+    int array3[8];
+
+    for (int i = 0; i < 5; i++)
+    {
+        array3[i] = array1[i];
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+
+        array3[i+5] = array2[i];
+    }
+    printf("array3 :");
+
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d", array3[i]);
+    }
+
+    return 0;
+}
